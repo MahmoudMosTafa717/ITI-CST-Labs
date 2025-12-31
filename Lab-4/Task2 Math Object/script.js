2ee49fca99e78ebafee0cff8c987a436625d9e2f
Mahmoud MosTafa <mhmodq717@gmail.com>
2025-12-31 14:40:53 +0200

    Lab#4

--- Lab-4/Task2 Math Object/script.js
+++ Lab-4/Task2 Math Object/script.js
@@ -6,5 +6,6 @@ var num = Number(prompt("Enter a number to get sqrt:"));
 alert("Sqrt = " + Math.sqrt(num));
 
 var ang = Number(prompt("Angle (degrees):"));
+
 var rad = ang * (Math.PI / 180);
 alert("Cos = " + Math.cos(rad));
